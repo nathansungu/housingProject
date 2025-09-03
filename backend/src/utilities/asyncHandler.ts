6ce6ff263a71ca4f1c04438f6272c09fd2585d36
nathansungu <nathanamudavi@gmail.com>
2025-09-03 22:21:13 +0300

    asynchronous handler utility

---
+++ backend/src/utilities/asyncHandler.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from "express";
+export const asyncHandler = (fn: Function) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        await fn(req, res, next);
+    } catch (err) {
+        next(err);
+    }
+};
