2bc03347f01e07889330231837d007996e5db776
nathansungu <nathanamudavi@gmail.com>
2025-09-30 10:23:07 +0300

    update routes index file"

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,9 +1,13 @@
 import auth from "./aut.route";
 import { houses } from "./houses.routes";
 import { Router } from "express";
+import { users } from "./users.route";
+import {message} from "../routes/messags.route"
 const router = Router();
 
 router.use("/auth", auth);
 router.use("/houses", houses)
+router.use("/users",users)
+router.use("/messages", message)
 
 export default router;
