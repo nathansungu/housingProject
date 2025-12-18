8176176a0fd4b7d6e90681efb63738ddf8557f71
nathansungu <nathanamudavi@gmail.com>
2025-12-18 22:39:52 +0300

    add cors handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,17 @@
 import express from "express";
 import router from "./routes/index.routes";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { errorHandler } from "./middlewares/errorHandler";
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use("/api", router);
 app.use(errorHandler);
 
